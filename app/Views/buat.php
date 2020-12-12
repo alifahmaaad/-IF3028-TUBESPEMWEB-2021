7e096b55379b4c264c6eb552c7371103565a0d00
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-12-11 19:42:32 -0800

    tampilan untuk simple lapor rev1

---
+++ app/Views/buat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buat</title>
+</head>
+<body>
+	<center><h1>SIMPLE LAPOR!</h1></center>
+	<br><br>
+	<p>Buat Laporan/Komentar</p>
+	<hr>
+	<br>
+	<div class="laporan">
+		Laporan/Komentar
+	</div>
+	<select>
+		<option>Aspek</option>
+	</select>
+	<input type="file" name="">
+</body>
+</html>
