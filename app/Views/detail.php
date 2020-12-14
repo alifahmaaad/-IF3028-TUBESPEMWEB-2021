29e06bf483c5495bbe0b5c1ac3f44144cf7f8bac
Muhammad Alfarizi Tazkia <61236352+MuhammadAlfariziTazkia@users.noreply.github.com>
2020-12-14 09:09:43 -0800

    menambah view untuk detail

---
+++ app/Views/detail.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buat</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<center><h1>SIMPLE LAPOR!</h1></center>
+	<br><br>
+	Detail Laporan/Komentar
+	<hr>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<br><br>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<br>
+	Lampiran:
+	<br><br>
+	<img src="img/example.png">
+	<br><br><br>
+	<div id="waktu"></div>
+	<div id="aspek"></div>
+	<a href="">Hapus Laporan/Komentar <img id="close" src="img/close.png"></a>
+	<br><br>
+	<hr>
+</body>
+</html>
